3128f900cf4bd761e9ef47e8379f7d107b5731d3
Stacy <52056904+stacyalexander@users.noreply.github.com>
2022-04-06 19:42:44 -0500

    Initial Commit
    Initial Commit

---
+++ Arm/StacyArm.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+from __future__ import division
+import time
+import RPi.GPIO as GPIO
+import sys
+import Adafruit_PCA9685
+import ultra as pos
+from camera import Camera
+from LED import LED
+from threading import Thread
+
+pwm = Adafruit_PCA9685.PCA9685()
+pwm.set_pwm_freq(50)
+led = LED
+
+# Initial pwm positions - set in init function
+pwm1_pos = 300;
+pwm2_pos = 300;
+pwm3_pos = 300;
+pwm4_pos = 175;
+
+def init():
+	# Set arm to center postion
+	pwm.set_pwm(0,0,400)
+	# Set arm to up postion
+	pwm.set_pwm(1,0,300)
+	# Set hand to up (out) postition
+	pwm.set_pwm(2,0,300)
+	# Open Gripper
+	pwm.set_pwm(3,0,300)
+	# Camera Down
+	pwm.set_pwm(4,0,175)
+	# Turn off LEDs
+	led.colorWipe(0, 0, 0)
+	
+def verify():
+	aquired = False
+	pwm.set_pwm(4,0,290)
+	time.sleep(2)
+	if (Camera.frames()):
+		print("Bag Aquired")
+		aquired = True
+		drop()
+	if not aquired:
+		pwm.set_pwm(4,0,320)
+		time.sleep(0.1)
+		if (Camera.frames()):
+			print("Bag Aquired")
+			aquired = True
+			drop()
+	if not aquired:
+		pwm.set_pwm(4,0,350)
+		time.sleep(.1)
+		if (Camera.frames()):
+			print("Bag Aquired")
+			aquired = True
+			drop()
+	if not aquired:
+		print("Try To Grab Bag Again!")
+	pwm.set_pwm(4,0,175)
+	pwm.set_pwm(3,0,300)
+	time.sleep(1)
+		
+def drop():
+	global pwm1_pos
+	global pwm2_pos
+	global pwm3_pos
+	# Open Gripper
+	for i in range(0,250):
+		pwm.set_pwm(3,0,(50+i))
+		pwm3_pos += 1;
+		time.sleep(0.005)
+	# Hand down
+	for i in range(0,150):
+		pwm.set_pwm(2,0,(420-i))
+		pwm2_pos -= 1;
+		time.sleep(0.005)
+	# Arm Down 
+	pwm.set_pwm(1,0,200)
+	time.sleep(1)
+	pwm.set_pwm(1,0,300)
+	
+
+def movearm():
+	global pwm1_pos
+	global pwm2_pos
+	global pwm3_pos
+	global completed
+	# Arm Down
+	for i in range(0,50):
+		pwm.set_pwm(1,0,(300-i))
+		pwm1_pos -= 1;
+		time.sleep(0.005)
+	# Hand out and Arm Down
+	for i in range(0,130):
+		pwm.set_pwm(2,0,(300+i))
+		pwm.set_pwm(1,0,(250-i))
+		pwm1_pos -= 1;
+		pwm2_pos += 1;
+		time.sleep(0.005)
+	# Close Gripper
+	for i in range(0,250):
+		pwm.set_pwm(3,0,(300-i))
+		pwm3_pos -= 1;
+		time.sleep(0.005)
+	# Arm Up	
+	for i in range(0,225):
+		pwm.set_pwm(1,0,(150+i))
+		pwm1_pos += 1;
+		time.sleep(0.005)
+	verify()
+
+if __name__ == '__main__':
+	#global completed
+	#t1 = Thread(target=movearm)
+	led = LED()
+	init()
+	while 1:
+		if pos.checkdist() < 0.11:
+			led.colorWipe(0, 0, 255)  # blue
+			results_file = open('results.csv', mode='a')
+			if (Camera.frames()):
+			#if pos.checkdist() > 0.08:
+				led.colorWipe(0, 255, 0)  # green
+				results_file.write("US Sensor," + str(pos.checkdist()) + "\n")
+				results_file.close()
+				movearm()
+			else:
+				print("Incorrect Color Detected")
+				led.colorWipe(255, 0, 0)  # red
+				time.sleep(.75)
+				led.colorWipe(0, 0, 0)
+				time.sleep(.75)
+				led.colorWipe(255, 0, 0)  # red
+				time.sleep(.75)
+			led.colorWipe(0, 0, 0)
+		#print(pos.checkdist())
+		time.sleep(0.05)
+	print("Exiting")
---
+++ Arm/camera.py
@@ -0,0 +1,60 @@
+import os
+import cv2
+from base_camera import BaseCamera
+import numpy as np
+import matplotlib.pyplot as plt
+
+import csv
+
+colorUpper = np.array([255, 100, 100])
+colorLower = np.array([175, 0, 0]) 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+class Camera(BaseCamera):  
+	video_source = 0  
+	def __init__(self):  
+		if os.environ.get('OPENCV_CAMERA_SOURCE'):  
+			Camera.set_video_source(int(os.environ['OPENCV_CAMERA_SOURCE']))  
+		super(Camera, self).__init__()
+		  
+	def set_video_source(source):  
+		Camera.video_source = source   
+	def frames():  
+		aquired = False
+		results_file = open('results.csv', mode='a')
+		camera = cv2.VideoCapture(Camera.video_source)  
+		if not camera.isOpened():  
+			raise RuntimeError('Could not start camera.') 
+		for i in range(0,50): 
+		# read current frame  
+			_, img = camera.read() #Obtain images captured by the camera  
+			hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  #Transfrom the images to HSV color space   
+			mask = cv2.inRange(hsv, colorLower, colorUpper) #Loop to detect the color based on the target color range in the HSV color space, and turn the color blocks into masks  
+			mask = cv2.erode(mask, None, iterations=2)  #Erode and diminish the small masks (hot pixels) in the image (eliminate small color blocks or hot pixels)  
+			mask = cv2.dilate(mask, None, iterations=2) #Dilate, to resize the large masks eroded in the previous line to the original  
+			cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,  
+			cv2.CHAIN_APPROX_SIMPLE)[-2]            #Find masks in the image  
+			center = None         
+			if len(cnts) > 0:   #If the number of masks is more than 1,  
+				# Find the coordinate of the center and size of the target color object in the image
+				c = max(cnts, key=cv2.contourArea)  
+				((box_x, box_y), radius) = cv2.minEnclosingCircle(c)  
+				M = cv2.moments(c)  
+				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))  
+				X = int(box_x)  
+				Y = int(box_y)  
+				# Obtain and output the coordinate of the center of the target color object 
+				print('X:%d'%X)  
+				print('Y:%d'%Y)  
+				print('-------')  
+				# Show the text "Target Detected" in the image 
+				cv2.putText(img,'Target Detected',(40,60), font, 0.5,(255,255,255),1,cv2.LINE_AA) 
+				print('--------- Color object detected --------- ')
+				results_file.write("Detected, " + str(X) + " " + str(Y) + "\n")
+				aquired = True
+				results_file.close()
+				return 1
+			else:  
+				cv2.putText(img,'Target Detecting',(40,60), font, 0.5,(255,255,255),1,cv2.LINE_AA)  
+		results_file.close()
+		return 0
